4287416446c6b19a6bbbc94e9c6c76ac62dfc453
dave <dave@wizmacau.com>
2019-07-18 12:48:26 +0800

    add sprite

--- plane.py
+++ plane.py
@@ -1,9 +1,40 @@
 # -*- coding:utf8 -*-
-import pygame
+from plane_sprites import *
+
 pygame.init()
 
-scree = pygame.display.set_mode()
+scree = pygame.display.set_mode([480, 700])
+
+bg = pygame.image.load("images/background.png")
+scree.blit(bg, (0, 0))
+
+hero = pygame.image.load("images/me1.png")
+scree.blit(hero, (150, 500))
+
+pygame.display.update()
+
+clock = pygame.time.Clock()
+hero_rect = pygame.Rect(150, 500, 102, 126)
+
+enemy = GameSprite("images/enemy1.png")
+enemy1 = GameSprite("images/enemy2.png", 2)
+enemy_group = pygame.sprite.Group(enemy, enemy1)
+
 while True:
-    pass
+    clock.tick(60)
+    for event in pygame.event.get():
+        print(event.type)
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    hero_rect.y -= 1
+    if hero_rect.y <= -126:
+        hero_rect.y = 700
+    scree.blit(bg, (0, 0))
+    scree.blit(hero, hero_rect)
+
+    enemy_group.update()
+    enemy_group.draw(scree)
+    pygame.display.update()
 
 pygame.quit()
