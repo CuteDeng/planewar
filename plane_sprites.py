5895af2b3cb6638f0e706acc46c6d786e72f76ad
dxw <1316896128@qq.com>
2019-07-21 16:42:22 +0800

    finish‘

--- plane_sprites.py
+++ plane_sprites.py
@@ -5,6 +5,7 @@ import pygame
 SCREEN_RECT = pygame.Rect(0, 0, 480, 700)
 FRAME_PER_SEC = 60
 CREATE_ENEMY_EVENT = pygame.USEREVENT
+HERO_FIRE_EVENT = pygame.USEREVENT + 1
 
 
 class GameSprite(pygame.sprite.Sprite):
@@ -41,10 +42,48 @@ class Enemy(GameSprite):
     def update(self):
         super().update()
         if self.rect.y >= SCREEN_RECT.height:
-            print("飞出屏幕")
+            # print("飞出屏幕")
             self.kill()
 
     def __del__(self):
-        print("敌机挂了")
+        # print("敌机挂了")
+        pass
+
+
+class Hero(GameSprite):
+    def __init__(self):
+        super().__init__("./images/me1.png", 0)
+        self.rect.centerx = SCREEN_RECT.centerx
+        self.rect.bottom = SCREEN_RECT.bottom - 120
+        self.bullets = pygame.sprite.Group()
+
+    def update(self):
+        self.rect.x += self.speed
+        if self.rect.x < 0:
+            self.rect.x = 0
+        elif self.rect.right > SCREEN_RECT.right:
+            self.rect.right = SCREEN_RECT.right
+
+    def fire(self):
+        print("发射子弹")
+        for i in (0, 1, 2):
+            bullet = Bullet()
+            bullet.rect.bottom = self.rect.y - i*20
+            bullet.rect.centerx = self.rect.centerx
+            self.bullets.add(bullet)
+
+
+class Bullet(GameSprite):
+    def __init__(self):
+        super().__init__("./images/bullet1.png", -2)
+
+    def update(self):
+        super().update()
+        if self.rect.bottom < 0:
+            self.kill()
+
+    def __del__(self):
+        print("子弹销毁")
+
 
 
