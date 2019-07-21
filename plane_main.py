d02a17bb2ed530b5c7938791c195ff741bac2dee
dxw <1316896128@qq.com>
2019-07-21 10:33:57 +0800

    绘制背景交替

--- plane_main.py
+++ plane_main.py
@@ -10,10 +10,38 @@ class PlaneGame(object):
         self.__create_sprites()
 
     def __create_sprites(self):
-        pass
+        bg1 = Background("images/background.png")
+        bg2 = Background("images/background.png")
+        bg2.rect.y = -bg2.rect.height
+        self.back_group = pygame.sprite.Group(bg1, bg2)
+
 
     def start_game(self):
         print("游戏开始")
+        while True:
+            self.clock.tick(FRAME_PER_SEC)
+            self.__event_handler()
+            self.__check_collide()
+            self.__update_sprites()
+            pygame.display.update()
+
+    def __event_handler(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                PlaneGame.__game_over()
+
+    def __check_collide(self):
+        pass
+
+    def __update_sprites(self):
+        self.back_group.update()
+        self.back_group.draw(self.screen)
+
+    @staticmethod
+    def __game_over():
+        print("游戏结束")
+        pygame.quit()
+        exit()
 
 
 if __name__ == '__main__':
--- plane_sprites.py
+++ plane_sprites.py
@@ -2,6 +2,7 @@
 import pygame
 
 SCREEN_RECT = pygame.Rect(0, 0, 480, 700)
+FRAME_PER_SEC = 60
 
 
 class GameSprite(pygame.sprite.Sprite):
@@ -13,4 +14,12 @@ class GameSprite(pygame.sprite.Sprite):
 
     def update(self):
         self.rect.y += self.speed
+
+
+class Background(GameSprite):
+    def update(self):
+        super().update()
+        if self.rect.y >= SCREEN_RECT.height:
+            self.rect.y = -self.rect.height
+
 
